d2bc98ddbe99ccf7c5361b66a02fa78e7073767f
Sayed Mahmudul Alam <smalam119@gmail.com>
2020-10-21 22:30:52 +0800

    [FEAT] toastStyle assigned on showToast

--- Toastly/Utils/Extensions/UIViewController+Extensions.swift
+++ Toastly/Utils/Extensions/UIViewController+Extensions.swift
@@ -9,7 +9,7 @@ import UIKit
 
 // MARK: - Extensions
 extension UIViewController {
-    public func showToast(message: String, position: ToastPositions = .top) {
+    public func showToast(message: String, position: ToastPositions = .top, toastStyle: ToastStyle = ToastStyle()) {
         let toastView = ToastView()
         toastView.setMessage(message)
         toastView.layoutIfNeeded()
@@ -17,6 +17,7 @@ extension UIViewController {
         let messageLabelHorizontalMargin = Constants.messageLabelTopConstraint + Constants.messageLabelBottomConstraint
         let toastPositionHelper = ToastPositionHelper(position: position, toastViewWidthHeight: (toastView.messageLabel.frame.width + messageLabelVerticalMargin, height: toastView.messageLabel.frame.height + messageLabelHorizontalMargin), navigationBarHeight: (self.navigationController?.navigationBar.frame.height), viewControllerFrame: self.view.frame)
         toastView.frame.origin = toastPositionHelper.getOrigin()
+        toastView.setStyle(toastStyle)
         self.view.addSubview(toastView)
     }
 }
