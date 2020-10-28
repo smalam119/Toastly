a174a2cf8c2f73fc55f217d0e59f7169fc152cd9
Sayed Mahmudul Alam <smalam119@gmail.com>
2020-10-28 21:36:43 +0800

    [FEAT] toast animation added.

--- Toastly/Utils/Extensions/UIViewController+Extensions.swift
+++ Toastly/Utils/Extensions/UIViewController+Extensions.swift
@@ -12,12 +12,18 @@ extension UIViewController {
     public func showToast(message: String, position: ToastPositions = .top, toastStyle: ToastStyle = ToastStyle()) {
         let toastView = ToastView()
         toastView.setMessage(message)
+        toastView.setStyle(toastStyle)
+        toastView.alpha = 1
         toastView.layoutIfNeeded()
         let messageLabelVerticalMargin = Constants.messageLabelLeadingConstraint + Constants.messageLabelTrailingConstraint
         let messageLabelHorizontalMargin = Constants.messageLabelTopConstraint + Constants.messageLabelBottomConstraint
         let toastPositionHelper = ToastPositionHelper(position: position, toastViewWidthHeight: (toastView.messageLabel.frame.width + messageLabelVerticalMargin, height: toastView.messageLabel.frame.height + messageLabelHorizontalMargin), navigationBarHeight: (self.navigationController?.navigationBar.frame.height), viewControllerFrame: self.view.frame)
         toastView.frame.origin = toastPositionHelper.getOrigin()
-        toastView.setStyle(toastStyle)
         self.view.addSubview(toastView)
+        UIView.animate(withDuration: 2.0, delay: 0.1, options: .curveEaseOut, animations: {
+            toastView.alpha = 0.0
+        }, completion: { (_) in
+            toastView.removeFromSuperview()
+        })
     }
 }
